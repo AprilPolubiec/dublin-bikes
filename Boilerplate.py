c731f57d57c82bdd66d450dc9b75b411242946fd
aida-coughlan <aidamariacoughlan@gmail.com>
2024-02-16 15:38:16 +0000

    Boilerplate for python file for scraper

---
+++ Boilerplate.py
@@ -0,0 +1,15 @@
+# import required stuff - see below
+# #import dbinfo
+# #import requests
+# #import json
+
+
+#We need a key
+
+#create get request
+
+#apply request to a variable
+#variable.text
+
+
+#create function to have request running until project is done and not requesting 
