65c91f256f66d506be8bb59b4fb1af5dccc2bb52
April Polubiec <aprilpolubiec@gmail.com>
2024-02-17 16:34:11 +0000

    first attempt at hitting rds

--- hello.py
+++ hello.py
@@ -1,8 +1,23 @@
-from flask import Flask
+import sqlalchemy as db
+import json
 
-app = Flask(__name__)
+# Opening JSON file
+f = open('secure/credentials.json')
+ 
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
 
+URI="dublin-bikes-db.cl020iymavvj.us-east-1.rds.amazonaws.com"
+PORT=3306
+DB="dublin-bikes"
+USER="admin"
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+engine = db.create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, data["DB_PASSWORD"], URI, PORT, DB), echo=True)
+print("Got engine")
+
+with engine.connect() as conn:
+    result = conn.execute("SHOW VARIABLES;")
+    engine.close()
+    for res in result:
+        print(res)
