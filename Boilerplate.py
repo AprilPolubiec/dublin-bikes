0d42e25e20d943b6b5e71b8916b76ca68e872a8c
aida-coughlan <aidamariacoughlan@gmail.com>
2024-02-18 12:59:55 +0000

    New key that worked for me when scraped data (I got it from inspecting the website)

--- Boilerplate.py
+++ Boilerplate.py
@@ -3,6 +3,35 @@
 # #import requests
 # #import json
 
+# import dbinfo
+import requests
+import json
+# Specify the API endpoint URL
+url = 'https://api.jcdecaux.com/vls/v3/stations?apiKey=frifk0jbxfefqqniqez09tw4jvk37wyf823b5j1i&contract=dublin'
+
+url = 'https://api.jcdecaux.com/vls/v3/stations?apiKey={api_key}&contract={contract_name}'
+# url = 'https://api.jcdecaux.com/vls/v1/stations?contract={Dublin} HTTP/1.1'
+headers = {
+    'Accept': 'application/json'
+}
+
+response = requests.get(url, headers=headers)
+
+
+# response = requests.get(url)
+# Check if the request was successful (status code 200)
+# if response.status_code == 200:
+#     # Print the response content
+#     print("Response Body:")
+#     print(response.json())  # Assuming the response is in JSON format
+# else:
+#     print("Failed to fetch data from the API. Status code:", response.status_code)
+if response.status_code == 200:
+    # Print the response content
+    print("Response Body:")
+    print(response.json())  # Assuming the response is in JSON format
+else:
+    print("Failed to fetch data from the API. Status code:", response.status_code)
 
 #We need a key
 
