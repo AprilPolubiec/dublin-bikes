a8c101ad597e0611eec34436e45ee7f09a440a5c
aida-coughlan <aidamariacoughlan@gmail.com>
2024-03-02 16:56:24 +0000

    Scraper format improved for storage in database.

--- dublin_bike_scraper.py
+++ dublin_bike_scraper.py
@@ -1,22 +1,21 @@
-# import required stuff - see below
-#import dbinfo
 import requests
 import json
+from datetime import datetime
+
+f = open("secure/credentials.json")
+data = json.load(f)
 
 # Specify the API endpoint URL
+url = "https://api.jcdecaux.com/vls/v3/stations?apiKey={}&contract=Dublin".format(data["key_2"])
 
-url = 'https://api.jcdecaux.com/vls/v3/stations?apiKey={api_key}&contract={contract_name}'
-
-headers = {
-    'Accept': 'application/json'
-}
+headers = {'Accept': 'application/json'}
 
 response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     # Print the response content
     print("Response Body:")
-    #print(response.json())  # Assuming the response is in JSON format
+    print(response.json())  # Assuming the response is in JSON format
 else:
     print("Failed to fetch data from the API. Status code:", response.status_code)
 
@@ -30,22 +29,21 @@ list_of_bike_stands =[item for item in json.loads(json_data)]
 # There are 114 different bike stands
 print(len(list_of_bike_stands))
 
-
 # List for filtering properties
-select_list = ['number', 'name', 'position', 'address', 'zip', 'contractName', 'banking', 'totalStands', 'status', 'mechanical_available', 'electric_available', 'stands_available', 'lastUpdate']
-second_list = ['longitude', 'latitude', 'capacity', 'stands']
+dict_keys = ['number', 'name', 'position', 'address', 'zip', 'contractName', 'banking', 'totalStands', 'status', 'mechanical_available', 'electric_available', 'stands_available', 'lastUpdate']
+innerDict_keys = ['longitude', 'latitude', 'capacity', 'stands', 'mechanicalBikes', 'electricalBikes']
 
 filtered_bike_stands = []
 
 #Loop through each bike stand for filtering (bike_stand is a dictionary)
 for bike_stand in list_of_bike_stands:
-    #Delete 'mainStands' because its the same as 'totalStands' TBC
+    #Delete 'mainStands' because its the same as 'totalStands' TBD
     del bike_stand['mainStands']
 
     # Create a temp dictionary called result which gets reset after each bikestand loop
     result = {}
     #Loops through each filtered property
-    for key in select_list:
+    for key in dict_keys:
         # If the bike stand contains the key property i.e we are interested in this 
         if key in bike_stand:
             # Checks if the value of the matched key is a dictionary (this applies for position and totalStands)
@@ -54,7 +52,7 @@ for bike_stand in list_of_bike_stands:
                 innerDict[key] = bike_stand[key]
                 # Loops through the values of the matched dictionary
                 for innerValue in innerDict.values():
-                    for key in second_list:
+                    for key in innerDict_keys:
                         # For totalStands, its value is yet another dictionary called availabilities
                         if 'availabilities' in innerValue:
                             availabilities = innerValue.get('availabilities')
@@ -75,12 +73,27 @@ for bike_stand in list_of_bike_stands:
     # After each bike-stand loop, we add the filtered properties to the list of all bike stands
     filtered_bike_stands.append(result)
 
-for i in filtered_bike_stands:
-    print(i)
+class BikeStand:
+    def __init__(self, number, name, longitude, latitude, address, contractName, banking, capacity, stands, mechanicalBikes, electricalBikes, status, lastUpdate):
+        self.number = number
+        self.name = name
+        self.longitude = longitude
+        self.latitude = latitude
+        self.address = address
+        self.city = contractName
+        self.accepts_cards = banking
+        self.total_stands = capacity
+        self.stands_available = stands
+        self.mechanical_available = mechanicalBikes
+        self.electric_available = electricalBikes
+        self.status = status
+        self.last_updated = datetime.timestamp(datetime.strptime(lastUpdate, "%Y-%m-%dT%H:%M:%SZ"))
 
-# Notes
-    # Cant see zip from the data
-    # is accept_cards = banking? (I think so)
-    # Unsure about mechanical/electrical_available too, is it electricalInternalBatteryBikes or just mechanical/electricalBikes? 
-    # If you figure this out, add key (string) property name to the second_list
-    # total stands = capacity btw
+formatted_bike_stands = []
+
+for i in filtered_bike_stands:
+    bike_stand = BikeStand(**i)
+    formatted_bike_stands.append(bike_stand)
+
+for bike_stand in formatted_bike_stands:
+    print(bike_stand)
