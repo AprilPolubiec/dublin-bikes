74fde9e9d2045a115fd061e77030e3989f7231ce
April Polubiec <aprilpolubiec@gmail.com>
2024-03-03 08:40:55 +0000

    make tests work

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ export DBIKE_DEV=True
 ```
 
 This creates an environment variable called "DBIKE_DEV" which we can then use at various parts in the code to determine whether somebody is running this script locally or from the EC2 instance.
+
+## Testing
+All tests are contained in the `tests` directory. To run tests, run the following command: `python3 -m unittest tests.[NAME_OF_TEST_FOLDER]`
+
+```
+<!-- Examples: -->
+python3 -m unittest tests.test_db_utils
+python3 -m unittest tests.test_utils
+```
--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -1,8 +1,9 @@
 import unittest
+from random import randrange
 from copy import deepcopy
 from web.db_utils import *
 
-class TestUtils(unittest.TestCase):
+class TestStationDBUtils(unittest.TestCase):
     filtered_api_response = {
             "number": 999,
             "name": "Aprils Stand New Name",
@@ -20,7 +21,6 @@ class TestUtils(unittest.TestCase):
             "last_updated": 9372934,
         }
     test_station_row = StationRow(filtered_api_response)
-    test_availability_row = AvailabilityRow(filtered_api_response)
 
     def setUp(self) -> None:
         disable_caching() # Disable caching unless explicitly testing it
@@ -30,189 +30,211 @@ class TestUtils(unittest.TestCase):
         conn.execute(sqla.delete(table).where(table.c.Id.in_(ids)))
         conn.commit()
 
-    def clean_up_availability(self, ids: list[int], table: sqla.Table):
-        conn.execute(sqla.delete(table).where(table.c.StationId.in_(ids)))
-        conn.commit()
-    
     def test_insert_station(self):
         print("==== TEST INSERT STATION ====")
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table) # Just in case
-        res = insert_station(self.test_station_row)
-        self.assertEqual(res, self.test_station_row)
+        station = deepcopy(self.test_station_row)
+        station.Id = randrange(1000, 2000)
+        res = insert_station(station)
+        self.assertEqual(res, station)
 
         # Check that it was added
         table = StationRow.table
 
-        stmnt = sqla.select(StationRow.table).where(table.c.Id == self.test_station_row.Id)
+        stmnt = sqla.select(StationRow.table).where(table.c.Id == station.Id)
         rows = conn.execute(stmnt).all()
         print("Got rows: ", rows)
         self.assertEqual(len(rows), 1)
         
-        self.assertEqual(StationRow(rows[0], True), self.test_station_row)
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table)
+        self.assertEqual(StationRow(rows[0], True), station)
+        self.clean_up_stations([station.Id], StationRow.table)
 
     def test_update_station(self):
         print("==== TEST UPDATE STATION ====")
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table) # Just in case
+        station = deepcopy(self.test_station_row)
+        station.Id = randrange(1000, 2000)
+    
+        self.clean_up_stations([station.Id], StationRow.table) # Just in case
 
         # Insert a station
         table = StationRow.table
-        conn.execute(sqla.insert(table), self.test_station_row.values())
+        conn.execute(sqla.insert(table), station.values())
         conn.commit()
 
         # Update the station name
-        updated_row = deepcopy(self.test_station_row)
+        updated_row = deepcopy(station)
         updated_row.Name = "Aprils Stand Even Newer Name"
         update_station(updated_row)
         
         # Check that it was updated to the same name
-        stmnt = sqla.select(StationRow.table).where(table.c.Id == self.test_station_row.Id)
+        stmnt = sqla.select(StationRow.table).where(table.c.Id == station.Id)
         rows = conn.execute(stmnt).all()
         self.assertEqual(len(rows), 1)
         
         self.assertEqual(StationRow(rows[0], True), updated_row)
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table)
+        self.clean_up_stations([station.Id], StationRow.table)
 
     def test_delete_station(self):
         print("==== TEST DELETE STATION ====")
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table) # Just in case
+        station = deepcopy(self.test_station_row)
+        station.Id = randrange(1000, 2000)
+        self.clean_up_stations([station.Id], StationRow.table) # Just in case
 
         # Insert a station
         table = StationRow.table
-        conn.execute(sqla.insert(table), self.test_station_row.values())
+        conn.execute(sqla.insert(table), station.values())
         conn.commit()
         
-        delete_station(self.test_station_row.Id)
+        delete_station(station.Id)
 
         # Check that it no longer exists
-        stmnt = sqla.select(StationRow.table).where(table.c.Id == 999)
+        stmnt = sqla.select(StationRow.table).where(table.c.Id == station.Id)
         rows = conn.execute(stmnt).all()
         self.assertEqual(len(rows), 0)
 
     def test_get_station(self):
         print("==== TEST GET STATION ====")
-        self.clean_up_stations([self.test_station_row.Id], StationRow.table) # Just in case
+        station = deepcopy(self.test_station_row)
+        station.Id = randrange(1000, 2000)
+        self.clean_up_stations([station.Id], StationRow.table) # Just in case
 
         # Insert a station
         table = StationRow.table
-        conn.execute(sqla.insert(table), self.test_station_row.values())
+        conn.execute(sqla.insert(table), station.values())
         conn.commit()
         
-        station = get_station(self.test_station_row.Id)
+        station = get_station(station.Id)
 
-        self.assertEqual(station, self.test_station_row)
+        self.assertEqual(station, station)
+        self.clean_up_stations([station.Id], StationRow.table)
+class TestAvailabilityDBUtils(unittest.TestCase):
+    filtered_api_response = {
+            "number": 999,
+            "name": "Aprils Stand New Name",
+            "latitude": 123,
+            "longitude": 456,
+            "address": "Main St",
+            "zip": "D9ghwf",
+            "city": "Dublin",
+            "accepts_cards": True,
+            "total_stands": 100,
+            "status": "OPEN",
+            "mechanical_available": 10,
+            "electric_available": 5,
+            "stands_available": 5,
+            "last_updated": 9372934,
+        }
+    test_availability_row = AvailabilityRow(filtered_api_response)
+    created_ids = []
+
+    def setUp(self) -> None:
+        disable_caching() # Disable caching unless explicitly testing it
+        return super().setUp()
     
+    def tearDown(self) -> None:
+        table = AvailabilityRow.table
+        conn.execute(sqla.delete(table).where(table.c.StationId.in_(self.created_ids)))
+        conn.commit()
+        return super().tearDown()
+
     def test_insert_availability(self):
         print("==== TEST INSERT AVAILABILITY ====")
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table) # Just in case
-        res = insert_availability(self.test_availability_row)
-        self.assertEqual(res, self.test_availability_row)
+        availability = deepcopy(self.test_availability_row)
+        availability.StationId = randrange(1000, 2000)
+        self.created_ids.append(availability.StationId)
+        print("test_insert_availability making station {}".format(availability.StationId))
+
+        res = insert_availability(availability)
+        self.assertEqual(res, availability)
 
         # Check that it was added
         table = AvailabilityRow.table
 
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == self.test_availability_row.StationId)
+        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == availability.StationId)
         rows = conn.execute(stmnt).all()
-        print("Got rows: ", rows)
         self.assertEqual(len(rows), 1)
         
-        self.assertEqual(AvailabilityRow(rows[0], True), self.test_availability_row)
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table)
-
-    def test_delete_single_availability(self):
-        print("==== TEST DELETE SINGLE AVAILABILITY ====")
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table) # Just in case
-
-        # Insert an availability
-        table = AvailabilityRow.table
-        conn.execute(sqla.insert(table), self.test_availability_row.values())
-        conn.commit()
-        
-        delete_availabilities(self.test_availability_row.StationId, self.test_availability_row.LastUpdated, self.test_availability_row.LastUpdated)
-
-        # Check that it no longer exists
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == 999)
-        rows = conn.execute(stmnt).all()
-        self.assertEqual(len(rows), 0)
+        self.assertEqual(AvailabilityRow(rows[0], True), availability)
 
     def test_delete_multiple_availabilities(self):
-        print("==== TEST DELETE SINGLE AVAILABILITY ====")
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table) # Just in case
+        print("==== TEST DELETE MULTIPLE AVAILABILITIES ====")
+        availability = deepcopy(self.test_availability_row)
+        availability.StationId = randrange(1000, 2000)
+        self.created_ids.append(availability.StationId)
+    
         table = AvailabilityRow.table
 
         # Insert 5 availabilities with last updated ranging from 1000 - 2000
         for timestamp in range(1000, 2001, 200):
-            availability = deepcopy(self.test_availability_row)
+            availability = deepcopy(availability)
             availability.LastUpdated = timestamp
             conn.execute(sqla.insert(table), availability.values())      
         
         conn.commit()
-        
         # Should delete two of the availabilities
-        res = delete_availabilities(self.test_availability_row.StationId, 1000, 1200)
+        res = delete_availabilities(availability.StationId, 1000, 1200)
         self.assertEqual(res, 2)
-    
-        # Check that only 3 availability rows are left
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == self.test_availability_row.StationId)
+
+        # Check that only 4 availability rows are left
+        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == availability.StationId)
         rows = conn.execute(stmnt).all()
-        self.assertEqual(len(rows), 3)
+        self.assertEqual(len(rows), 4)
     
         # Try deleting availabilities for a non-existent Station
         res = delete_availabilities(12345, 1000, 1200)
-        self.assertEqual(len(res), 0)
+        self.assertEqual(res, 0)
     
         # Check that still only 3 availability rows are left
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == self.test_availability_row.StationId)
+        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == availability.StationId)
         rows = conn.execute(stmnt).all()
-        self.assertEqual(len(rows), 3)
+        self.assertEqual(len(rows), 4)
     
         # Try deleting availabilities for timestamps that don't exist
-        res = delete_availabilities(12345, 2200, 4000)
-        self.assertEqual(len(res), 0)
+        res = delete_availabilities(availability.StationId, 2200, 4000)
+        self.assertEqual(res, 0)
     
-        # Check that still only 3 availability rows are left
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == self.test_availability_row.StationId)
+        # Check that still only 4 availability rows are left
+        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == availability.StationId)
         rows = conn.execute(stmnt).all()
-        self.assertEqual(len(rows), 3)
+        self.assertEqual(len(rows), 4)
     
         # Try deleting availabilities for a single timestamp
-        res = delete_availabilities(12345, 1800, 1800)
-        self.assertEqual(len(res), 1)
+        res = delete_availabilities(availability.StationId, 1800, 1800)
+        self.assertEqual(res, 1)
     
-        # Check that 2 availability rows are left
-        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == self.test_availability_row.StationId)
+        # Check that 3 availability rows are left
+        stmnt = sqla.select(AvailabilityRow.table).where(table.c.StationId == availability.StationId)
         rows = conn.execute(stmnt).all()
-        self.assertEqual(len(rows), 2)
+        self.assertEqual(len(rows), 3)
 
     def test_get_availability(self):
         print("==== TEST GET AVAILABILITY ====")
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table) # Just in case
+        availability = deepcopy(self.test_availability_row)
+        availability.StationId = randrange(1000, 2000)
+        self.created_ids.append(availability.StationId)
         table = AvailabilityRow.table
 
         # Insert 5 availabilities with last updated ranging from 1000 - 2000
         for timestamp in range(1000, 2001, 200):
-            availability = deepcopy(self.test_availability_row)
+            availability = deepcopy(availability)
             availability.LastUpdated = timestamp
             conn.execute(sqla.insert(table), availability.values())      
         
         conn.commit()
 
-        availability = get_availability(self.test_availability_row.StationId, 1000, 2000)
-        self.assertEqual(len(availability), 6)
+        availability_rows = get_availability(availability.StationId, 1000, 2000)
+        self.assertEqual(len(availability_rows), 6)
 
-        availability = get_availability(self.test_availability_row.StationId, 1000, 1000)
-        self.assertEqual(len(availability), 1)
+        availability_rows = get_availability(availability.StationId, 1000, 1000)
+        self.assertEqual(len(availability_rows), 1)
 
-        availability = get_availability(self.test_availability_row.StationId, None, None)
-        self.assertEqual(len(availability), 6)
+        availability_rows = get_availability(availability.StationId, None, None)
+        self.assertEqual(len(availability_rows), 6)
 
-        availability = get_availability(self.test_availability_row.StationId, 1400, None)
-        self.assertEqual(len(availability), 4)
+        availability_rows = get_availability(availability.StationId, 1400, None)
+        self.assertEqual(len(availability_rows), 4)
     
-        availability = get_availability(self.test_availability_row.StationId, None, 1400)
-        self.assertEqual(len(availability), 3)
-
-        self.clean_up_availability([self.test_availability_row.StationId], AvailabilityRow.table)
+        availability_rows = get_availability(availability.StationId, None, 1400)
+        self.assertEqual(len(availability_rows), 3)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
