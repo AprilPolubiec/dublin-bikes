3d1bf6acf66769249f7652b3fc447ed0650c9e0f
aida-coughlan <aidamariacoughlan@gmail.com>
2024-03-05 14:19:14 +0000

    def get_weather() created

--- weather_api.py
+++ weather_api.py
@@ -2,36 +2,47 @@
 import requests
 import json
 
-# Replace 'YOUR_API_KEY' with your actual API key
-city = 'Dublin'
-latitude = 53.3498
-longitude = -6.2672
-url = f'https://api.openweathermap.org/data/2.5/weather'
+import requests
+import json
 
-f = open("secure/credentials.json")
-data = json.load(f)
-W_API = data["W_API"]
+def get_weather():
+    city = 'Dublin'
+    latitude = 53.3498
+    longitude = -6.2672
+    url = 'https://api.openweathermap.org/data/2.5/weather'
 
-# Make the API request
-response = requests.get(url, params={"units":"metric", "lat": latitude, "lon":longitude, "appid":W_API})
+    # Load API key from credentials file
+    with open("secure/credentials.json") as f:
+        data = json.load(f)
+        W_API = data["W_API"]
 
-# Check if request was successful
-if response.status_code == 200:
-    # Parse JSON response
-    data = response.json()
-    
-    # Extract relevant weather information
-    temperature = data['main']['temp']
-    humidity = data['main']['humidity']
-    wind_speed = data['wind']['speed']
-    rain = data['weather'][0]['description']
+    # Make the API request
+    response = requests.get(url, params={"units": "metric", "lat": latitude, "lon": longitude, "appid": W_API})
 
+    # Check if request was successful
+    if response.status_code == 200:
+        # Parse JSON response
+        data = response.json()
 
-    # Print weather information
-    print(f'Weather in {city}:')
-    print(f'Temperature: {temperature} K')
-    print(f'Humidity: {humidity}%')
-    print(f'Wind Speed: {wind_speed} m/s')
-    print(f'Forecast: {rain}')
-else:
-    print('Failed to retrieve weather data')
+        # Extract relevant weather information
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        rain = data['weather'][0]['description']
+
+        # Print weather information
+        print(f'Weather in {city}:')
+        print(f'Temperature: {temperature} °C')
+        print(f'Humidity: {humidity}%')
+        print(f'Wind Speed: {wind_speed} m/s')
+        print(f'Forecast: {rain}')
+    else:
+        print('Failed to retrieve weather data')
+
+# Call the function to get weather information
+get_weather()
+
+# great job!! last thing to do is wrap all of this code into a function that we can call in our scraper script. So like
+#     def get_weather():
+#   ## All the code here
+    # That way in our scraper script we can call "get_weather()" - message me if you have questions!!
