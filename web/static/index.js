a572b4633ff567a1e1322ccfb4e9065ff563089a
April Polubiec <aprilpolubiec@gmail.com>
2024-03-05 07:36:25 +0000

    delete pycache and nonsense code

--- web/static/index.js
+++ web/static/index.js
@@ -2,30 +2,17 @@ const DUBLIN_LATITUDE = 53.3498;
 const DUBLIN_LONGITUDE = 6.2603;
 
 function initMap() {
-const dublinCoordinates = { lat: DUBLIN_LATITUDE, lng: DUBLIN_LONGITUDE };
-const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: dublinCoordinates,
-});
+    const dublinCoordinates = { lat: DUBLIN_LATITUDE, lng: DUBLIN_LONGITUDE };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: dublinCoordinates,
+    });
 
-new google.maps.Marker({
-    position: dublinCoordinates,
-    map,
-    title: "Hello World!",
-});
-}
-(response) => {
-return response;
-}
-
-(response) => response;
-
-(response) => {
-return response;
-}
-
-function doThing(response){
-return response;
+    new google.maps.Marker({
+        position: dublinCoordinates,
+        map,
+        title: "Hello World!",
+    });
 }
 
 window.initMap = initMap;
