f62b2bd07d9c3b53da01ff369a8f27497cdcc384
April Polubiec <aprilpolubiec@gmail.com>
2024-03-06 09:23:13 +0000

    add info windows

--- web/static/index.js
+++ web/static/index.js
@@ -6,36 +6,39 @@ const DUBLIN_LONGITUDE = 6.2603;
 async function initMap() {
     const dublinCoordinates = { lat: DUBLIN_LATITUDE, lng: DUBLIN_LONGITUDE };
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 6,
         center: dublinCoordinates,
     });
-
+  
+    var markerBounds = new google.maps.LatLngBounds();
     const stations = await getStations();
     for (const station of stations) {
-        const lat = parseFloat(station.PositionLatitude);
-        const lng = parseFloat(station.PositionLongitude);
+        const position = new google.maps.LatLng(parseFloat(station.PositionLatitude), parseFloat(station.PositionLongitude))
         const marker = new google.maps.Marker({
-            position: { lat, lng },
+            position,
             map,
             title: station.Name,
         });
 
-        // const contentString =
-        //     '<div id="bodyContent">' +
-        //     "<p><b>Availability: </b> 0 " +
-        //     "</div>";
-      // const infowindow = new google.maps.InfoWindow({
-      //   content: contentString,
-      //   ariaLabel: station.Name,
-      // });
+        const contentString =
+            '<div class="infowindow-content">' +
+            "<p><b>Availability: </b> 0 " +
+            "</div>";
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        ariaLabel: station.Name,
+      });
 
-      // marker.addListener("click", () => {
-      //   infowindow.open({
-      //     anchor: marker,
-      //     map,
-      //   });
-      // });
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+        });
+      });
+      markerBounds.extend(position)
     }
+
+    map.fitBounds(markerBounds);
 
 }
 
